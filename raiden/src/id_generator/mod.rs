8b0f2264552703f6677f4e80458c1663311d3d9e
bokuweb <bokuweb12@gmail.com>
2020-07-01 10:50:17 +0900

    fix: Fix uuid by env variable

--- raiden/src/id_generator/mod.rs
+++ raiden/src/id_generator/mod.rs
@@ -1,7 +1,10 @@
 pub trait IdGenerator {
     #[cfg(not(test))]
     fn gen() -> String {
-        uuid::Uuid::new_v4().to_string()
+        match std::env::var("RAIDEN_UUID_FIXED_BY") {
+            Ok(v) => v,
+            Err(_) => uuid::Uuid::new_v4().to_string(),
+        }
     }
 
     #[cfg(test)]
@@ -9,4 +12,3 @@ pub trait IdGenerator {
         "01234567-89ab-cdef-0123-456789abcdef".to_owned()
     }
 }
-
