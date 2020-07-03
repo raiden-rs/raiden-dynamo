d1f082099fffbe10909c621a9345fd4276b80a4a
bokuweb <bokuweb12@gmail.com>
2020-07-03 12:11:48 +0900

    spec: Add test

---
+++ raiden/tests/all/transact_write.rs
@@ -0,0 +1,43 @@
+#[cfg(test)]
+mod tests {
+
+    #[cfg(test)]
+    use pretty_assertions::assert_eq;
+    use raiden::*;
+
+    #[derive(Raiden)]
+    #[raiden(table_name = "user")]
+    #[derive(Debug, Clone)]
+    pub struct User {
+        #[raiden(partition_key)]
+        id: String,
+        name: String,
+    }
+
+    #[test]
+    fn test_minimum_transact_write() {
+        let tx = ::raiden::WriteTx::new(Region::Custom {
+            endpoint: "http://localhost:8000".into(),
+            name: "ap-northeast-1".into(),
+        });
+        let cond = User::condition().attr_not_exists(UserAttrNames::Id);
+        let input = User::put_item_builder()
+            .id("testId".to_owned())
+            .name("bokuweb".to_owned())
+            .build()
+            .unwrap();
+        let input2 = User::put_item_builder()
+            .id("testId2".to_owned())
+            .name("bokuweb".to_owned())
+            .build()
+            .unwrap();
+        assert_eq(
+            tx.put(User::put(input).condition(cond))
+                .put(User::put(input2))
+                .run()
+                .await
+                .is_ok(),
+            true,
+        )
+    }
+}
