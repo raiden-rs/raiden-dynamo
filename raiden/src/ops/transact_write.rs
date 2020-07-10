89fb60da90dc45c3659c2d52663eb3b07ae084ec
bokuweb <bokuweb12@gmail.com>
2020-07-11 03:14:27 +0900

    feat: Add update for txWrite for now

--- raiden/src/ops/transact_write.rs
+++ raiden/src/ops/transact_write.rs
@@ -23,6 +23,16 @@ impl WriteTx {
         self
     }
 
+    pub fn update(mut self, builder: impl TransactWriteUpdateBuilder) -> Self {
+        self.items.push(TransactWriteItem {
+            condition_check: None,
+            delete: None,
+            update: Some(builder.build()),
+            put: None,
+        });
+        self
+    }
+
     pub async fn run(self) -> Result<(), crate::RaidenError> {
         let _res = self
             .client
@@ -34,10 +44,15 @@ impl WriteTx {
             })
             .await?;
         // TODO: ADD Response later
+        dbg!(&_res);
         Ok(())
     }
 }
 
 pub trait TransactWritePutBuilder {
     fn build(self) -> crate::Put;
+}
+
+pub trait TransactWriteUpdateBuilder {
+    fn build(self) -> crate::Update;
 }
