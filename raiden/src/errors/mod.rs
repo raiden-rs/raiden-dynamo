1a13f463d5a981ca3e0cf266f6b00e999e1ecf86
rchaser53 <tayoshizawa29@gmail.com>
2020-07-14 20:43:39 +0900

    feat: Add error converter for ScanError

--- raiden/src/errors/mod.rs
+++ raiden/src/errors/mod.rs
@@ -118,6 +118,27 @@ impl From<RusotoError<QueryError>> for RaidenError {
     }
 }
 
+impl From<RusotoError<ScanError>> for RaidenError {
+  fn from(error: RusotoError<ScanError>) -> Self {
+      match error {
+          RusotoError::Service(error) => match error {
+              ScanError::InternalServerError(msg) => RaidenError::InternalServerError(msg),
+              ScanError::ProvisionedThroughputExceeded(msg) => {
+                  RaidenError::ProvisionedThroughputExceeded(msg)
+              }
+              ScanError::RequestLimitExceeded(msg) => RaidenError::RequestLimitExceeded(msg),
+              ScanError::ResourceNotFound(msg) => RaidenError::ResourceNotFound(msg),
+          },
+          RusotoError::HttpDispatch(e) => RaidenError::HttpDispatch(e),
+          RusotoError::Credentials(e) => RaidenError::Credentials(e),
+          RusotoError::Validation(msg) => RaidenError::Validation(msg),
+          RusotoError::ParseError(msg) => RaidenError::ParseError(msg),
+          RusotoError::Unknown(res) => RaidenError::Unknown(res),
+          RusotoError::Blocking => RaidenError::Blocking,
+      }
+  }
+}
+
 impl From<RusotoError<PutItemError>> for RaidenError {
     fn from(error: RusotoError<PutItemError>) -> Self {
         match error {
