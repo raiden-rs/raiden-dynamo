ead5b738087e0f1532df0b0a3d37e7782cba1f75
bokuweb <bokuweb12@gmail.com>
2020-10-21 20:09:05 +0900

    fix: clippy error

--- raiden-derive/src/ops/shared.rs
+++ raiden-derive/src/ops/shared.rs
@@ -31,69 +31,17 @@ pub(crate) fn expand_attr_to_item(
                 }
               },
             }
-        } else {
-            if use_default {
-                let ty = &f.ty;
-                quote! {
-                  #ident: {
-                    let item = #item_ident.get(#attr_key);
-                    if item.is_none() {
-                        #ty::default()
-                    } else {
-                        let converted = ::raiden::FromAttribute::from_attr(item.cloned());
-                        if converted.is_err() {
-                          // TODO: improve error handling.
-                            return Err(::raiden::RaidenError::AttributeConvertError{ attr_name: #attr_key.to_string() });
-                        }
-                        converted.unwrap()
-                    }
-                  },
-                }
-            } else {
-                quote! {
-                    #ident: {
-                      let item = #item_ident.get(#attr_key);
-                      let converted = ::raiden::FromAttribute::from_attr(item.cloned());
-                      if converted.is_err() {
-                        // TODO: improve error handling.
-                          return Err(::raiden::RaidenError::AttributeConvertError{ attr_name: #attr_key.to_string() });
-                      }
-                      converted.unwrap()
-                    },
-                  }
-            }
-        }
-    }).collect()
-}
-
-/*
-use quote::*;
-
-pub(crate) fn expand_attr_to_item(
-    item_ident: &proc_macro2::Ident,
-    fields: &syn::FieldsNamed,
-    rename_all_type: crate::rename::RenameAllType,
-) -> Vec<proc_macro2::TokenStream> {
-    fields.named.iter().map(|f| {
-        let ident = &f.ident.clone().unwrap();
-        let renamed = crate::finder::find_rename_value(&f.attrs);
-        let use_default = crate::finder::include_unary_attr(&f.attrs, "use_default");
-        let attr_key  = if let Some(renamed) = renamed {
-            renamed
-        }  else if rename_all_type != crate::rename::RenameAllType::None {
-            crate::rename::rename(rename_all_type, ident.to_string())
-        } else {
-            ident.to_string()
-        };
-        if crate::finder::is_option(&f.ty) {
+        } else if use_default {
+            let ty = &f.ty;
             quote! {
               #ident: {
                 let item = #item_ident.get(#attr_key);
                 if item.is_none() {
-                    None
+                    #ty::default()
                 } else {
                     let converted = ::raiden::FromAttribute::from_attr(item.cloned());
                     if converted.is_err() {
+                      // TODO: improve error handling.
                         return Err(::raiden::RaidenError::AttributeConvertError{ attr_name: #attr_key.to_string() });
                     }
                     converted.unwrap()
@@ -101,38 +49,17 @@ pub(crate) fn expand_attr_to_item(
               },
             }
         } else {
-            let field_type = format_ident!("{}", stringify!(&f.ty));
-            if use_default {
-                quote! {
-                    #ident: {
-                        let item = #item_ident.get(#attr_key);
-                        if item.is_none() {
-                            #field_type::default()
-                        } else {
-                            let converted = ::raiden::FromAttribute::from_attr(item.cloned());
-                            if converted.is_err() {
-                                // TODO: improve error handling.
-                                return Err(::raiden::RaidenError::AttributeConvertError{ attr_name: #attr_key.to_string() });
-                            }
-                            converted.unwrap()
-                        }
-                    },
-                }
-            } else {
-              quote! {
-                    #ident: {
-                        let item = #item_ident.get(#attr_key);
-                        let converted = ::raiden::FromAttribute::from_attr(item.cloned());
-                        if converted.is_err() {
-                            // TODO: improve error handling.
-                            return Err(::raiden::RaidenError::AttributeConvertError{ attr_name: #attr_key.to_string() });
-                        }
-                        converted.unwrap()
-                    },
-                }
-            }
+            quote! {
+                #ident: {
+                  let item = #item_ident.get(#attr_key);
+                  let converted = ::raiden::FromAttribute::from_attr(item.cloned());
+                  if converted.is_err() {
+                    // TODO: improve error handling.
+                      return Err(::raiden::RaidenError::AttributeConvertError{ attr_name: #attr_key.to_string() });
+                  }
+                  converted.unwrap()
+                },
+              }
         }
     }).collect()
 }
-
-*/
