a71bf245ba361f98c179fb0712a9c5e480934f1b
Yusuke Tanaka <yusuktan@maguro.dev>
2021-09-19 01:52:35 +0900

    Add more tests for batch_delete

--- raiden/tests/all/batch_delete.rs
+++ raiden/tests/all/batch_delete.rs
@@ -1,5 +1,5 @@
 #[cfg(test)]
-mod tests {
+mod partition_key_tests {
 
     #[cfg(test)]
     use pretty_assertions::assert_eq;
@@ -33,6 +33,116 @@ mod tests {
                     unprocessed_items: vec![],
                 }
             );
+        }
+        rt.block_on(example());
+    }
+
+    #[test]
+    fn test_batch_delete_item_for_stored_and_unstored_keys() {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        async fn example() {
+            let client = BatchDeleteTest0::client(Region::Custom {
+                endpoint: "http://localhost:8000".into(),
+                name: "ap-northeast-1".into(),
+            });
+
+            let res = client.batch_delete(vec!["id3", "unstored"]).run().await;
+            assert!(res.is_ok());
+        }
+        rt.block_on(example());
+    }
+
+    #[test]
+    fn test_batch_delete_item_for_unstored_keys() {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        async fn example() {
+            let client = BatchDeleteTest0::client(Region::Custom {
+                endpoint: "http://localhost:8000".into(),
+                name: "ap-northeast-1".into(),
+            });
+
+            let res = client
+                .batch_delete(vec!["unstored0", "unstored1", "unstored2"])
+                .run()
+                .await;
+            assert!(res.is_ok());
+        }
+        rt.block_on(example());
+    }
+}
+
+#[cfg(test)]
+mod partition_key_and_sort_key_tests {
+    #[cfg(test)]
+    use raiden::*;
+
+    #[derive(Raiden, Debug, Clone, PartialEq)]
+    pub struct BatchDeleteTest1 {
+        #[raiden(partition_key)]
+        id: String,
+        name: String,
+        #[raiden(sort_key)]
+        year: usize,
+    }
+
+    #[test]
+    fn test_batch_delete_item_with_sort_key() {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        async fn example() {
+            let client = BatchDeleteTest1::client(Region::Custom {
+                endpoint: "http://localhost:8000".into(),
+                name: "ap-northeast-1".into(),
+            });
+
+            let res = client
+                .batch_delete(vec![
+                    ("id0", 1999_usize),
+                    ("id1", 2000_usize),
+                    ("id2", 2001_usize),
+                ])
+                .run()
+                .await;
+            assert!(res.is_ok());
+        }
+        rt.block_on(example());
+    }
+
+    #[test]
+    fn test_batch_delete_item_with_sort_key_for_stored_and_unstored_keys() {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        async fn example() {
+            let client = BatchDeleteTest1::client(Region::Custom {
+                endpoint: "http://localhost:8000".into(),
+                name: "ap-northeast-1".into(),
+            });
+
+            let res = client
+                .batch_delete(vec![("id3", 2002_usize), ("unstored", 2000_usize)])
+                .run()
+                .await;
+            assert!(res.is_ok());
+        }
+        rt.block_on(example());
+    }
+
+    #[test]
+    fn test_batch_delete_item_with_sort_key_for_unstored_keys() {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        async fn example() {
+            let client = BatchDeleteTest1::client(Region::Custom {
+                endpoint: "http://localhost:8000".into(),
+                name: "ap-northeast-1".into(),
+            });
+
+            let res = client
+                .batch_delete(vec![
+                    ("unstored0", 1999_usize),
+                    ("unstore1", 2000_usize),
+                    ("unstored2", 2001_usize),
+                ])
+                .run()
+                .await;
+            assert!(res.is_ok());
         }
         rt.block_on(example());
     }
--- setup/setup.ts
+++ setup/setup.ts
@@ -15,7 +15,8 @@ import { project } from "./fixtures/project.ts";
 import { batchTest0 } from "./fixtures/batch_test_0.ts";
 import { batchTest1 } from "./fixtures/batch_test_1.ts";
 import { batchTest2 } from "./fixtures/batch_test_2.ts";
-import { batchDeleteTest0 } from "./fixtures/batch_delete.ts";
+import { batchDeleteTest0 } from "./fixtures/batch_delete_test_0.ts";
+import { batchDeleteTest1 } from "./fixtures/batch_delete_test_1.ts";
 import { testUserStaging } from "./fixtures/test_user_staging.ts";
 import { deleteTest0 } from "./fixtures/delete_test_0.ts";
 import { deleteTest1 } from "./fixtures/delete_test_1.ts";
@@ -58,6 +59,7 @@ const data = [
   batchTest1,
   batchTest2,
   batchDeleteTest0,
+  batchDeleteTest1,
   testUserStaging,
   deleteTest0,
   deleteTest1,
