e6ca7e0b582ace39f735e1cd71f0416aadafb0e3
bokuweb <bokuweb12@gmail.com>
2024-06-04 15:10:17 +0900

    fix: pub

--- raiden-derive/src/ops/update.rs
+++ raiden-derive/src/ops/update.rs
@@ -97,7 +97,7 @@ pub(crate) fn expand_update_item(
             // #(#output_fields)*
         }
 
-        struct #update_expression_name;
+        pub struct #update_expression_name;
 
         impl #struct_name {
             pub fn update_expression() -> #update_expression_name {
